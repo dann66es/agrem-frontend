450b3cae23f71a3b5e9d6b1080ca99c3217bb6d6
yozzy <janellispatricia@gmail.com>
2025-05-27 17:23:36 -0500

    Styles for Logo, Header, and Title edited. Photos added.

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,12 @@ function App() {
         <Header/>
 
         <h1 className="h1">Asociación Para la Gestión del Riesgo Empresarial</h1>
+
+           <div className="WorldWin">
+
+              <img src="/Photo2.jpg" id="PhotoWidth"  alt="Photo" />  
+        
+           </div>   
         
       </div>
      
